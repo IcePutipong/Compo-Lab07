137acd1e4695fe7cb5fa234e684455cb07618607
Putipong Sailen <tyice789@gmail.com>
2023-10-08 11:33:15 +0700

    Lab9_7: not finish

--- src/main/java/se331/lab/rest/entity/ParticipantOwnEventDTO.java
+++ src/main/java/se331/lab/rest/entity/ParticipantOwnEventDTO.java
@@ -5,18 +5,20 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class EventDTO {
+public class ParticipantOwnEventDTO {
     Long id;
     String category;
     String title;
     String description;
+    String location;
     String date;
     String time;
     Boolean petAllowed;
-    EventOrganizerDTO organizer;
-    EventParticipantDTO participant;
+    List<Participant> participants;
 }
